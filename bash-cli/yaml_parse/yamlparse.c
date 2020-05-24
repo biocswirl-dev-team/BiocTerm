7294d26651ac724cccf69f7b7c997bbe5632b73a
lisancao <lisanatashacao@gmail.com>
2020-05-23 17:55:02 -0700

    [feat][WIP] Add basic Bash YAML parsing
    Being able to read in bash variables actively while maintaining biocswirl course structure could be the best path to Terminal Courses
    
    Example: Github issue #8

---
+++ bash-cli/yaml_parse/yamlparse.c
@@ -0,0 +1,17 @@
+/******************************************************
+ * File: yaml_parse.c
+ * Description: This file uses the c yaml parser library to read course materials
+ *
+ * Author: Lisa Cao
+ * Written: 05/23/2020
+ * 
+ ******************************************************/
+
+
+
+#include <yaml.h> 
+
+#include <stdlib.h> 
+#include <stdio.h> 
+
+
