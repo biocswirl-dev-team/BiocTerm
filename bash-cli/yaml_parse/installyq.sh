5b77de6cdd3e4341a329a54baddf946f2b46415c
lisancao <lisanatashacao@gmail.com>
2021-03-28 14:24:12 -0700

    [version][intf] Add CSHL Hackathon code
    - Completely refactor and add in new golang code based on cobra
    - old code from CSHL hackathon last year

--- bash-cli/yaml_parse/installyq.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys CC86BB64
-sudo add-apt-repository ppa:rmescandon/yq
-sudo apt update
-sudo apt install yq -y
---
+++ libraries.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# get cobra
+go get -u github.com/spf13/cobra/cobra
+
+
